acbd11702bd820699058755c398d0e3b66ce4d0e
bylbaboy <khvalexey@gmail.com>
2023-09-08 17:31:09 +0400

    Update ZooModules.cs

--- Assets/Scripts/Modules/ZooModules.cs
+++ Assets/Scripts/Modules/ZooModules.cs
@@ -10,7 +10,9 @@ namespace Modules
         protected override List<IModule> GetModules() =>
             new()
             {
-                new AnimalSpawnerModule(new RandomAnimalPicker(), new ConstantValuesProvider<int>(3000)),
+                new AnimalSpawnerModule(
+                    new RandomAnimalPicker(), 
+                    new BoundedValuesProvider<int>(new Bounds<int>(1000, 2000), new RandomIntPicker())),
             };
 
         protected override List<IService> GetServices() =>
